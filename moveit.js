c6049e69779fc9bd37429791a265185441a03577
kiran JD <kiranjd8@gmail.com>
2017-10-08 01:50:30 +0530

    ignore

--- moveit.js
+++ moveit.js
@@ -1,16 +1,12 @@
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
 function change_bg() {
-  var bg1 = ["https://i.stack.imgur.com/yEQ6k.jpg","https://previews.123rf.com/images/frankrohde/frankrohde1105/frankrohde110500208/9443736-Abstract-grunge-background-pattern-for-your-text-Stock-Photo-texture.jpg","https://static.pexels.com/photos/547114/pexels-photo-547114.jpeg"];
-  var string = bg1[getRandomArbitrary(bg1.length-1, bg1.length)];
-  var w1 = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
-var h1 = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
-alert(w1);
-  document.body.style.background = "url('" + string + "') no-repeat ";
-  document.body.style.backgroundSize = "100%";
+  var bg1 = ["https://i.stack.imgur.com/yEQ6k.jpg", "https://previews.123rf.com/images/frankrohde/frankrohde1105/frankrohde110500208/9443736-Abstract-grunge-background-pattern-for-your-text-Stock-Photo-texture.jpg", "https://static.pexels.com/photos/547114/pexels-photo-547114.jpeg"];
+  var string = bg1[getRandomArbitrary(bg1.length - 1, bg1.length)];
+  document.body.style.background = "url('" + string + "') no-repeat fixed";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center center"
+
 }

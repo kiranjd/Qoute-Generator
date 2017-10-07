4ddf922a807ee17e056930c56e7bee381896b057
kiran JD <kiranjd8@gmail.com>
2017-10-08 01:32:52 +0530

    random-bg-imag done

--- moveit.js
+++ moveit.js
@@ -1,21 +1,16 @@
-function image
-{
-  return url(https://tpc.googlesyndication.com/simgad/7074644680100628249);
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-
-    var bgSrc1 = "http://urlpic1.jpg";
-    var bgSrc2 = "http://urlpic2.jpg";
-    var bgElem = document.getElementById('showcase-row');
-    var num = getRandomArbitrary(0,1);
-
-    function getRandomArbitrary(min, max) {
-      return Math.random() * (max - min) + min;
-    }
-    if(num >= 0.5){
-        bgElem.style.backgroundImage = "url('" + bgSrc1 + "')";
-        bgElem.style.backgroundPosition = "0 -80px";
-    }else{
-        bgElem.style.backgroundImage = "url('" + bgSrc2 + "')";
-    }
-});
+function change_bg() {
+  var bg1 = ["https://i.stack.imgur.com/yEQ6k.jpg","https://previews.123rf.com/images/frankrohde/frankrohde1105/frankrohde110500208/9443736-Abstract-grunge-background-pattern-for-your-text-Stock-Photo-texture.jpg","https://static.pexels.com/photos/547114/pexels-photo-547114.jpeg"];
+  var string = bg1[getRandomArbitrary(bg1.length-1, bg1.length)];
+  var w1 = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+var h1 = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+alert(w1);
+  document.body.style.background = "url('" + string + "') no-repeat ";
+  document.body.style.backgroundSize = "100%";
+}

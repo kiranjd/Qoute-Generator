994895b4b2ce4c338943a23ffcb74926b3088ec6
kiran JD <kiranjd8@gmail.com>
2017-10-10 03:02:04 +0530

    quotes display

--- moveit.js
+++ moveit.js
@@ -1,12 +1,16 @@
+$(document).ready(function(){
+ $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+  $("body").append("<p>" + a[0].content +"</p>"+ "<p >&mdash;" + a[0].title +  "</p>")
+});
+});
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 function change_bg() {
   var bg1 = ["https://i.stack.imgur.com/yEQ6k.jpg", "https://previews.123rf.com/images/frankrohde/frankrohde1105/frankrohde110500208/9443736-Abstract-grunge-background-pattern-for-your-text-Stock-Photo-texture.jpg", "https://static.pexels.com/photos/547114/pexels-photo-547114.jpeg"];
-  var string = bg1[getRandomArbitrary(bg1.length - 1, bg1.length)];
+  var string = bg1[getRandomArbitrary(0, bg1.length)];
   document.body.style.background = "url('" + string + "') no-repeat fixed";
   document.body.style.backgroundSize = "cover";
-  document.body.style.backgroundPosition = "center center"
-
+  document.body.style.backgroundPosition = "center center";
 }

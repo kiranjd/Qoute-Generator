4b99d0164da9f4627043001af2076019c97be27a
kiran JD <kiranjd8@gmail.com>
2017-10-12 01:51:56 +0530

    jQuery Animation DONE

--- moveit.js
+++ moveit.js
@@ -1,20 +1,26 @@
+var first=true;
 function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 function sample() {
-   $("p").remove();
+   first=false;
    $(document).ready(function(){
+    $("p").remove();
    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]="+getRandomArbitrary(1,30)+"&callback=", function(a) {
-         $("body").append("<p>" + a[0].content +"</p>"+ "<p >&mdash;" + a[0].title +  "</p>")
+         $("body").append("<p id=\"para\">" + a[0].content +"</p>"+ "<p >&mdash;" + a[0].title +  "</p>")
          });
       });
 }
 function change_bg() {
-   //$("p").remove();
-   sample();
+   if(first)
+      sample();
   var bg1 = ["https://i.stack.imgur.com/yEQ6k.jpg", "https://previews.123rf.com/images/frankrohde/frankrohde1105/frankrohde110500208/9443736-Abstract-grunge-background-pattern-for-your-text-Stock-Photo-texture.jpg", "https://static.pexels.com/photos/547114/pexels-photo-547114.jpeg"];
   var string = bg1[getRandomArbitrary(0, bg1.length)];
   document.body.style.background = "url('" + string + "') no-repeat fixed";
   document.body.style.backgroundSize = "cover";
   document.body.style.backgroundPosition = "center center";
+}
+function anim() {
+      $("body").animate({top:"100%"});
+      $("body").animate({top:"0%"});
 }
